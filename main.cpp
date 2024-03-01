389a021aa6f2b2a8f57a08bf1e3293e3f093d66f
lekksha_ <lekksha@gmail.com>
2024-03-01 16:27:35 +0700

    convert to main window

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "widget.h"
-
+#include <QFileDialog>
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+#include "mainwindow.h"
+
+int main(int argc, char** argv) {
+    QApplication app{ argc, argv };
+    MainWindow window;
+    window.show();
+
+    return app.exec();
 }
